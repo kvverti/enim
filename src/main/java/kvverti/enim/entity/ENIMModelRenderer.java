2a82092f871663f430c6531857f92e144402b739
kvverti <1050cn@gmail.com>
2016-05-29 19:18:32 -0400

    Child element translation fix

--- src/main/java/kvverti/enim/entity/ENIMModelRenderer.java
+++ src/main/java/kvverti/enim/entity/ENIMModelRenderer.java
@@ -57,6 +57,8 @@ public final class ENIMModelRenderer extends ModelRenderer {
 			if(translucent) makeLucent();
 			GlStateManager.callList(displayList());
 			if(translucent) endLucent();
+			GlStateManager.translate(-rotationPointX * scale, -rotationPointY * scale, -rotationPointZ * scale);
+			GlStateManager.translate(-offsetX, -offsetY, -offsetZ);
 			if(childModels != null) childModels.forEach(box -> box.render(scale * defaultScale));
 			GlStateManager.popMatrix();
 		}
